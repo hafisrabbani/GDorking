51ebdd2615bf670dfdc3590741d31508b1c40217
MarsHallSec <47811548+X-MarsHall@users.noreply.github.com>
2020-12-18 19:45:22 +0700

    Update dork.php

--- dork.php
+++ dork.php
@@ -11,14 +11,14 @@ echo "\n";
 $dork = urlencode($dor);
 
 
-for ($i = 2;$i <= 10;$i++){
+for ($i = 2;$i <= 100;$i++){
 
 
       $sa = curl_init();
             curl_setopt($sa, CURLOPT_URL, "https://www.google.com/search?q=$dork");
             curl_setopt($sa, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($sa, CURLOPT_FOLLOWLOCATION, 1);
-   	        curl_setopt($sa, CURLOPT_USERAGENT, "Google Bot");
+   	    curl_setopt($sa, CURLOPT_USERAGENT, "Google Bot");
             curl_setopt($sa, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($sa, CURLOPT_SSL_VERIFYHOST, 0);
             $result = curl_exec($sa); 
