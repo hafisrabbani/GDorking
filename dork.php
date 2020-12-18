41ab6343fa8f310d53e47181de6f267f8e7dc998
MarsHallSec <47811548+X-MarsHall@users.noreply.github.com>
2020-12-18 19:44:42 +0700

    Add files via upload

---
+++ dork.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(0);
+
+echo "\nGoogle Dorking\n";
+echo "Created By MarsHall\n\n";
+
+
+echo "Input Dork >> ";
+$dor = trim(fgets(STDIN));
+echo "\n";
+$dork = urlencode($dor);
+
+
+for ($i = 2;$i <= 10;$i++){
+
+
+      $sa = curl_init();
+            curl_setopt($sa, CURLOPT_URL, "https://www.google.com/search?q=$dork");
+            curl_setopt($sa, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($sa, CURLOPT_FOLLOWLOCATION, 1);
+   	        curl_setopt($sa, CURLOPT_USERAGENT, "Google Bot");
+            curl_setopt($sa, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($sa, CURLOPT_SSL_VERIFYHOST, 0);
+            $result = curl_exec($sa); 
+
+preg_match_all('/a href="(.*?)"/i', $result, $a);
+
+echo $a[1][$i];
+echo "\n";
+
+}
+
+?>
